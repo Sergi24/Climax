9c9c544958c006f8faf2350680b152db7fb5d573
Sergi24 <34821737+Sergi24@users.noreply.github.com>
2019-05-16 19:27:56 +0200

    Spike modified

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
+    public float rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Spike : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
 }
